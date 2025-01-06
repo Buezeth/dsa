f91c11cf094b3f27e0628ab473c176ea2dfd6d2e
Buezeth <chibuezeth@gmail.com>
2025-01-06 04:34:23 -0800

    Impliment shift and unshift method

--- code.js
+++ code.js
@@ -43,10 +43,39 @@ class LinkedList {
         this.length--
         return temp
     }
+
+    unshift(value) {
+        const newNode = new Node(value)
+
+        if(!this.head) {
+            this.head = newNode
+            this.tail = newNode
+           }
+
+        let temp = this.head
+        newNode.next = temp
+
+        this.head = newNode
+        this.length ++
+    }
+
+    shift() {
+        let temp = this.head
+        this.head = this.head.next
+        temp.next = null
+
+        if(this.length === 0) {
+            this.tail === null
+        }
+
+        return temp
+    }
 }
 
 const myLinkedList = new LinkedList(1)
 myLinkedList.push(10)
 myLinkedList.push(3)
-myLinkedList.pop()
+// myLinkedList.pop()
+myLinkedList.unshift(6)
+myLinkedList.shift()
 console.log(myLinkedList)
